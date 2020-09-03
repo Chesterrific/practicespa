bf0fbb5cfb6dfb735d647887c81982048bc52243
Chester Guinto <chesterguinto@gmail.com>
2020-09-02 18:05:51 -0700

    Updates

---
+++ precache-manifest.cbc23a4edd04563650b60fa42c6bfaf0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbd4edd3d926f508737dd6e8ee94aa50",
+    "url": "/practicespa/index.html"
+  },
+  {
+    "revision": "d79bdc7e22a6b084d741",
+    "url": "/practicespa/static/css/main.d2fcf11a.chunk.css"
+  },
+  {
+    "revision": "f13d613455b1ad8aa1ba",
+    "url": "/practicespa/static/js/2.6d405054.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/practicespa/static/js/2.6d405054.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d79bdc7e22a6b084d741",
+    "url": "/practicespa/static/js/main.652401b6.chunk.js"
+  },
+  {
+    "revision": "04a547b61667c7c56915",
+    "url": "/practicespa/static/js/runtime-main.dec085fc.js"
+  }
+]);
